0c3f70a951b22b79a5f9461a45c6f176d37f1e04
unknown <sofibarbiero@gmail.com>
2022-10-14 11:43:06 -0300

    Practica proyecto consola

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Entidades;
+using Negocio;
+
+Console.WriteLine("BIBLIOTECA FABRICARG");
+
+Servicio gestor = new Servicio();
+List<Autor> lstAutor = gestor.ObtenerAutores();
+
+Console.WriteLine("Los autores disponibles son:");
+
+foreach(Autor autor in lstAutor)
+{
+    Console.WriteLine("Nombre completo: " + autor.Nombre + ", " + autor.Apellido
+        + " | Pais de nacimiento: " + autor.PaisNac);
+}
+
+
